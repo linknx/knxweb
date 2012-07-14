4b2f6e6e8c3a2106672e5414b2571b565eb5de3b
energy01 <energy01>
2012-07-14 11:40:14 +0000

    bug fixed on path of jquerymobile

--- previewmobile.php
+++ previewmobile.php
@@ -3,11 +3,13 @@
 require_once("include/common.php");
 require_once("include/linknx.php");
 
-tpl()->addCss('lib/jquery-mobile/jquery.mobile-1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.css');
+//tpl()->addCss('lib/jquery-mobile/jquery.mobile-1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.css');
+tpl()->addCss('lib/jquery-mobile/jquery.mobile-1.0b1.css');
 
 tpl()->addJs('lib/jquery/js/jquery.min.js');
 tpl()->addJs('js/design_mobile.js');
-tpl()->addJs('lib/jquery-mobile/jquery.mobile-1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.js');
+//tpl()->addJs('lib/jquery-mobile/jquery.mobile-1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.js');
+tpl()->addJs('lib/jquery-mobile/jquery.mobile-1.0b1.js');
 
 tpl()->display('design_mobile.tpl');
 
